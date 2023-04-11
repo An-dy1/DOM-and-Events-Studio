60622a6f52aa2eb00b53bb486dc7d92296a82a9f
andie <andrea_sue2@icloud.com>
2023-04-10 19:00:07 -0500

    solution

--- scripts.js
+++ scripts.js
@@ -1,2 +1,37 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+function moveRocket(direction) {
+  const rocket = document.getElementById('rocket');
+  const shuttleBackground = document.getElementById('shuttleBackground');
+
+  const backgroundWidth = shuttleBackground.offsetWidth;
+  const backgroundHeight = shuttleBackground.offsetHeight;
+
+  let bottom = parseInt(rocket.style.bottom) || 0;
+  let left = parseInt(rocket.style.left) || 0;
+
+  if (direction === 'up' && bottom <= backgroundHeight - 75) {
+    bottom += 10;
+  } else if (direction === 'down' && bottom >= 10) {
+    bottom -= 10;
+  } else if (direction === 'left' && left >= 10) {
+    left -= 10;
+  } else if (direction === 'right' && left <= backgroundWidth - 75) {
+    left += 10;
+  }
+
+  rocket.style.bottom = `${bottom}px`;
+  rocket.style.left = `${left}px`;
+}
+
+function runApp() {
+  const upButton = document.getElementById('up');
+  const downButton = document.getElementById('down');
+  const leftButton = document.getElementById('left');
+  const rightButton = document.getElementById('right');
+
+  upButton.addEventListener('click', () => moveRocket('up'));
+  downButton.addEventListener('click', () => moveRocket('down'));
+  leftButton.addEventListener('click', () => moveRocket('left'));
+  rightButton.addEventListener('click', () => moveRocket('right'));
+}
+
+window.addEventListener('load', runApp);
